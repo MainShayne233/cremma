59bbf074dac66bc650ae8e7766d514bd05d18322
MainShayne233 <shaynetremblay@hotmail.com>
2017-05-27 18:58:11 -0400

    adds another import test for function importing

--- __tests__/imports_test.js
+++ __tests__/imports_test.js
@@ -1,9 +1,11 @@
 import { expect } from 'chai'
-import Shed, { object } from '../src/index'
+import Shed, { object } from '../src'
+import { equivalent } from '../src/object'
 
 describe('imports of library', () => {
   it ('should be able to import everything', () => {
     expect(Shed).to.not.equal(undefined)
     expect(object).to.not.equal(undefined)
+    expect(equivalent).to.not.equal(undefined)
   })
 })
