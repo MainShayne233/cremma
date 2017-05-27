1ec1a1edbaff98c606989ca6e7af393c4a185415
MainShayne233 <shaynetremblay@hotmail.com>
2017-05-27 18:43:03 -0400

    adds array test for object.equivalent

---
+++ __tests__/__support__/helpers/fixtures/arrays.js
@@ -0,0 +1,28 @@
+export const sampleArray1 = [
+  'woah',
+  'array?',
+  'sweet!',
+  {
+    now: {
+      a: {
+        nested: 'object'
+      }
+    }
+  },
+  'I am the kaboose!',
+]
+
+export const sampleArray2 = [
+  1,
+  1,
+  2,
+  3,
+  5,
+  6,
+  {
+    err: 'not the fib sequence',
+    oh: {
+      well: ['that is okay'],
+    },
+  },
+]
--- __tests__/object_test.js
+++ __tests__/object_test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 import { object } from '../src'
 import { sampleObject1, sampleObject2 } from './__support__/helpers/fixtures/objects'
+import { sampleArray1, sampleArray2 } from './__support__/helpers/fixtures/arrays'
 
 describe('object', () => {
 
@@ -10,6 +11,12 @@ describe('object', () => {
       const clonedObject = JSON.parse( JSON.stringify(sampleObject1) )
       expect( object.equivalent(sampleObject1, clonedObject) ).to.equal(true)
       expect( object.equivalent(sampleObject1, sampleObject2) ).to.equal(false)
+    })
+
+    it('should also work for arrays', () => {
+      const clonedArray = JSON.parse( JSON.stringify(sampleArray1) )
+      expect( object.equivalent(sampleArray1, clonedArray) ).to.equal(true)
+      expect( object.equivalent(sampleArray1, sampleArray2) ).to.equal(false)
     })
   })
 })
