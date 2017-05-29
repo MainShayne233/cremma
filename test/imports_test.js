c781e1b1ce178965d205f06624c9f046dd0250fc
MainShayne233 <shaynetremblay@hotmail.com>
2017-05-29 03:26:40 -0400

    adds more to import test

--- test/imports_test.js
+++ test/imports_test.js
@@ -1,11 +1,13 @@
 import { expect } from 'chai'
-import Cremma, { object } from '../src'
+import Cremma, { object, string, array } from '../src'
 import { equivalent } from '../src/object'
 
 describe('imports of library', () => {
   it ('should be able to import everything', () => {
     expect(Cremma).to.not.equal(undefined)
     expect(object).to.not.equal(undefined)
+    expect(string).to.not.equal(undefined)
+    expect(array).to.not.equal(undefined)
     expect(equivalent).to.not.equal(undefined)
   })
 })
